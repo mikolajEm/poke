08bcb0f18deff16232a2a425c9d1b6333dda34e6
Miko M <mikolaj@mikolajm.pl>
2019-04-30 14:01:03 +0200

    404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { NavComponent } from './nav/nav.component';
+
+import { NotfoundComponent } from './notfound/notfound.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent},  
-  { path: 'details/:id', component: HomeComponent}
+  { path: 'details/:id', component: HomeComponent},
+  {path: '404', component: NotfoundComponent},
+  {path: '**', redirectTo: '404'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { ModalComponent } from './modal/modal.component';
 import { CardComponent } from './card/card.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 
 
@@ -25,7 +26,8 @@ import { CardComponent } from './card/card.component';
     HomeComponent,    
     FooterComponent, ModalComponent,
     InfiniteScrollerDirective,
-    CardComponent
+    CardComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
